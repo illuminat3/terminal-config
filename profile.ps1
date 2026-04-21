3433d61a66cfa76f2f211338169c48cca2e66326
Matthew <matthewrawson2@gmail.com>
2026-04-21 10:06:06 +0100

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -2,6 +2,7 @@ Import-Module posh-git
 oh-my-posh init pwsh --config (Join-Path $env:pwshConfig 'theme.omp.json') | Invoke-Expression
 
 . (Join-Path $env:pwshConfig 'Open-Solution.ps1')
+. (Join-Path $env:pwshConfig 'Fuzzy-Cd.ps1')
 
 function git {
     if ($args[0] -eq 'cleanup') {
